200831dbadf00e8f50e3986247266ba925ed86b4
Saurabh SHarma <saurshaz@gmail.com>
2016-08-26 16:41:40 +0530

    cleanup and integrating auth widget

--- code/front/src/index.js
+++ code/front/src/index.js
@@ -2,14 +2,8 @@ const express = require('express')
 const session = require('express-session')
 const path = require('path')
 const dao = require('./dao/')
-const mongodb = require('mongodb');
-const async = require('async')
-const router = express.Router()
-const GridStore = require('mongodb').GridStore
-const favicon = require('serve-favicon')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
-const request = require('request')
 const ejs = require('ejs')
 const app = express()
 const env = require('./lib/environment')
@@ -18,7 +12,6 @@ const utils = require('./lib/utils')
 const superagent = require('superagent')
 let socket_io = require('socket.io')
 require('newrelic')
-
 
 // @todo
 // have default groups setup, ability to add new
@@ -38,23 +31,23 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.engine('html', ejs.renderFile)
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended:true }))
 app.use(cookieParser())
 app.use(session({
-  secret: 'i8a@l9p#o0p$v1le2ea3rp4rp5ol6ce7kr8s',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: true,
-    maxAge: new Date(Date.now() + 3600000)
+  secret:'i8a@l9p#o0p$v1le2ea3rp4rp5ol6ce7kr8s',
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    secure:true,
+    maxAge:new Date(Date.now() + 3600000)
   }
 }))
-app.use(express.static(path.join(__dirname,  'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 const clear = (res) => {
-  res.clearCookie('loggedinuserid');
-  res.clearCookie('applerac');
-  res.clearCookie('auth');
+  res.clearCookie('loggedinuserid')
+  res.clearCookie('applerac')
+  res.clearCookie('auth')
   res.redirect(process.env.EXECUTOR_URL)
 }
 
@@ -64,44 +57,44 @@ app.get('/logout', (req, res) => {
 
 app.get('/get_user', (req, res) => {
   req.session.userInfo = req.cookies
-  if(req.session.userInfo && req.session.userInfo.loggedinuserid){
+  if (req.session.userInfo && req.session.userInfo.loggedinuserid) {
     superagent.get(`http://${process.env.AUTH_HOST}/decode_user?applerac=${req.cookies.applerac}`)
     .set('http_referer', process.env.EXECUTOR_URL)
     .set('referer', process.env.EXECUTOR_URL)
-    .end( (err, response) => {
-        if(err){
-         log.error('error ', err)
-         res.json({err: err})
-        }else{
-         res.json(response.body)
-        }
+    .end((err, response) => {
+      if (err) {
+        log.error('error ', err)
+        res.json({err:err})
+      } else {
+        res.json(response.body)
+      }
     })
   } else {
     // clear(res)
-    res.json({err: 'nothing in session'})
+    res.json({err:'nothing in session'})
   }
 })
 
 app.get('/', (req, res) => {
   req.session.userInfo = req.cookies
-  res.render('index', {REDIRECT_URI_PATH: process.env.EXECUTOR_URL, AUTH_SERVER_URL: process.env.AUTH_SERVER_URL, userInfo: req.session.userInfo})
+  res.render('index', {REDIRECT_URI_PATH:process.env.EXECUTOR_URL, AUTH_SERVER_URL:process.env.AUTH_SERVER_URL, userInfo:req.session.userInfo})
 })
 
 app.put('/persist', (req, res) => {
   req.session.userInfo = req.cookies
-  if(!(req.session.userInfo && req.session.userInfo.loggedinuserid)){// @todo :: add session checking/token checking
-    res.json({err: 'err', detail: 'not logged in'})
+  if (!(req.session.userInfo && req.session.userInfo.loggedinuserid)) { // @todo :: add session checking/token checking
+    res.json({err:'err', detail:'not logged in'})
   } else {
     dao.saveFunction(env._filesdb, req.body, req.session.userInfo, (err, fileData) => {
       if (err) {
         log.info({
-            'message': 'writefile -- writing to GridStore -- err',
-            'text': err
-        });
-        res.json({err:err});
+          'message':'writefile -- writing to GridStore -- err',
+          'text':err
+        })
+        res.json({err:err})
       } else {
         // Flush to the GridFS
-        res.json({err: err, data: fileData})
+        res.json({err:err, data:fileData})
       }
     })
   }
@@ -116,19 +109,19 @@ app.put('/fly', (req, res) => {
     if (!err) {
       res.json(results)
     } else {
-      res.json({err: err, detail: ' error in executing code'})
+      res.json({err:err, detail:' error in executing code'})
     }
-  });
+  })
 })
 
 let server = app.listen(process.env.PORT, () => {
   log.info('Executor Server is running at -', process.env.PORT)
 })
 app.io.attach(server)
-app.io.sockets.on('connection', function (socket) {
+app.io.on('connection', function (socket) {
   log.info('*** Connection made : >>>> socket query received >>>> ', socket.request._query)
   socket.emit('it_started', {'it':'started'})
+  socket.emit('xxx', {'xxx':'yyy'})
 })
-app.io.emit('xxx', {'xxx':'yyy'})
 // start websocket stuff
 // sockets_stuff(app.io)
--- code/front/src/public/usermanagement.tag
+++ code/front/src/public/usermanagement.tag
@@ -37,6 +37,6 @@
     </script>
 
     <style>
-        
+
     </style>
 </usermanagement>
